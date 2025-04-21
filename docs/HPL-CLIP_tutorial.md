098531731b4ad5918831b020759c1e29e33fdbe5
yumibriones <yumibriones@gmail.com>
2025-04-21 15:59:59 -0400

    added script for CLIP and tutorial

---
+++ docs/HPL-CLIP_tutorial.md
@@ -0,0 +1,40 @@
+# HPL-CLIP tutorial
+
+How to run HPL-CLIP. This is an integration of https://github.com/mlfoundations/open_clip and https://github.com/AdalbertoCq/Histomorphological-Phenotype-Learning.
+
+## Training HPL-CLIP
+
+1. Clone the open_clip repo to your directory of choice: https://github.com/mlfoundations/open_clip.
+
+2. From this HPL-Modified repo, open `scripts/train_clip.sh`. Edit as needed (e.g., activate correct conda environment, specify correct path/to/open/clip, edit `train-data`, etc.).
+
+3. Submit `scripts/train_clip.sh` as a batch job. A TensorBoard log should appear in the open_clip log directory (e.g., `/gpfs/home/yb2612/dl4med_25/dl_project/results/logs/`).
+
+4. Check for the TensorBoard log in the log directory (e.g.,`events.out.tfevents.1745262456.gpu-0003.121832.0`).
+
+5. Forward port (e.g., 9199) from your remote machine to your local machine.
+
+```
+ssh -L 9199:localhost:9199 bigpurple
+```
+
+6. Activate a conda environment which has TensorBoard installed (e.g., `dl4med_25`).
+
+```
+conda activate dl4med_25
+```
+
+7. Run TensorBoard and point it to the log directory that contains the event files:
+
+```
+tensorboard --logdir /gpfs/home/yb2612/dl4med_25/dl_project/results/logs/ --port 9199 --host 0.0.0.0
+```
+
+This should return something like:
+
+```
+TensorFlow installation not found - running with reduced feature set.
+TensorBoard 2.19.0 at http://0.0.0.0:9199/ (Press CTRL+C to quit)
+```
+
+8. Open the link from the output above in a web browser (e.g., http://0.0.0.0:9199/). This opens the TensorBoard interface, which contains training metrics, graphs, etc.
---
+++ scripts/train_clip.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --partition=gpu8_medium                  # Node
+#SBATCH --nodes=2
+#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=10GB                     # Memory limit
+#SBATCH --wait-all-nodes=1
+#SBATCH --job-name=train_clip_250k         # Job name
+#SBATCH --output=/gpfs/home/yb2612/dl4med_25/dl_project/results/logs/%x_%j.out  # Redirect stdout log to logs directory
+#SBATCH --error=/gpfs/home/yb2612/dl4med_25/dl_project/results/logs/%x_%j.err   # Redirect stderr log to logs directory
+#SBATCH --time=48:00:00                       # Max time for short
+
+# activate conda venv
+source /gpfs/data/hammelllab/yumi/lib/yumi_miniconda/etc/profile.d/conda.sh
+conda activate dl4med_25
+echo "conda activated"
+
+echo "Using Python at: $(which python)"
+
+# navigate to cloned open_clip repo
+cd /gpfs/home/yb2612/dl4med_25/dl_project/open_clip
+
+export MASTER_PORT=12802
+
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+
+srun --cpu_bind=v --accel-bind=gn python -u src/open_clip_train/main.py \
+    --save-frequency 1 \
+    --report-to tensorboard \
+    --train-data="/gpfs/home/yb2612/dl4med_25/dl_project/data/lung_250k_filepath_caption.csv" \
+    --csv-img-key filepath \
+    --csv-caption-key caption \
+    --csv-separator , \
+    --logs /gpfs/home/yb2612/dl4med_25/dl_project/results/logs \
+    --warmup 2000 \
+    --batch-size=128 \
+    --lr=1e-3 \
+    --wd=0.1 \
+    --epochs=30 \
+    --workers=4 \
+    --model ViT-B-32 \
+    --name "HPL-ViT-B-32_train_250k" \
+    --seed 9 \
+    --local-loss \
+    --gather-with-grad
